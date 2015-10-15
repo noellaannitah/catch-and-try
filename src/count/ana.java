dd3d992bb798959cbbc15c7b5d3741655f5da54a
abdousaanyani@gmail.com <abdousaanyani@yahoo.fr>
2015-10-15 14:30:16 -0700

    annitah catch and try work

---
+++ src/count/ana.java
@@ -0,0 +1,29 @@
+package count;
+
+public class ana {
+
+	public static void main(String[] args)
+	{
+
+	        try{
+	            
+	        } catch(Exception ex){
+	            System.out.println("Inside 1st catch Block");
+	        } finally {
+	            System.out.println("Inside 1st finally block");
+	        }
+	       
+	        try{
+	          
+	        } catch(Exception ex){
+	            System.out.println("Inside 2nd catch Block");
+	        } finally {
+	            System.out.println("Inside 2nd finally block");
+	        }
+	    }
+	
+
+
+	}
+
+
